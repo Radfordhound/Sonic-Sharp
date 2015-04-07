0c7d3e96f7e7805b7433da9c2d9a2b89e645b9da
Radfordhound <Radfordhound@users.noreply.github.com>
2015-04-07 18:33:53 -0500

    Started adding animations (Horribly set-up ATM!)

--- SonicSharp/SonicSharp/Player.cs
+++ SonicSharp/SonicSharp/Player.cs
@@ -14,6 +14,11 @@ namespace SonicSharp
         public int id; //The id of the player. For example, 1 for player 1, 2 for player 2, etc.
         public double x = 0;
         public double y = 0;
+        public int currentframe = 0;
+        public Vector2 origin = new Vector2(0, 0);
+        public float angle = 0;
+        public animationstate animstate = animationstate.idle;
+        public bool left = false;
         public double xsp; //Player's X Speed
         public double ysp; //Player's Y Speed
 
@@ -24,10 +29,15 @@ namespace SonicSharp
         public int top = 6;
 
         //Textures
-        public Texture2D running;
+        public List<Texture2D> idle = new List<Texture2D>();
+        public List<Texture2D> walking = new List<Texture2D>();
+        public List<Texture2D> running = new List<Texture2D>();
+        public Texture2D tex;
 
         public void Update(GameTime gameTime)
         {
+            //angle = MathHelper.ToRadians(-30); 
+
             KeyboardState kbst = Keyboard.GetState();
 
             if (kbst.IsKeyDown(Keys.Left) || kbst.IsKeyDown(Keys.A))
@@ -44,6 +54,7 @@ namespace SonicSharp
                 {
                     xsp = -top;
                 }
+                left = true;
             }
             else if (kbst.IsKeyDown(Keys.Right) || kbst.IsKeyDown(Keys.D))
             {
@@ -59,6 +70,7 @@ namespace SonicSharp
                 {
                     xsp = top;
                 }
+                left = false;
             }
             else
             {
@@ -75,12 +87,236 @@ namespace SonicSharp
 
         public void Draw()
         {
-            Main.spriteBatch.Draw(running, new Vector2((float)x, 70));
+            if (!left)
+            {
+                Main.spriteBatch.Draw(tex, new Vector2((float)x, 20), null, null, origin, angle, null, null, SpriteEffects.None);
+            }
+            else
+            {
+                Main.spriteBatch.Draw(tex, new Vector2((float)x, 20), null, null, origin, angle, null, null, SpriteEffects.FlipHorizontally);
+            }
+
+            Animate();
 
             if (this.GetType() == typeof(Sonic))
             {
                 //Custom Sonic-only logic goes here.
             }
+        }
+
+        public void GetAnimState()
+        {
+            if (xsp >= 6 || xsp <= -6)
+            {
+                if (animstate != animationstate.running)
+                {
+                    animstate = animationstate.running;
+                    currentframe = 0;
+                }
+            }
+            else if (xsp != 0)
+            {
+                if (animstate != animationstate.walking)
+                {
+                    animstate = animationstate.walking;
+                    currentframe = 0;
+                }
+            }
+            else
+            {
+                if (animstate != animationstate.idle)
+                {
+                    animstate = animationstate.idle;
+                    currentframe = 0;
+                }
+            }
+        }
+
+        public void GetFrames()
+        {
+            //
+        }
+
+        public void Animate()
+        {
+            GetAnimState();
+
+            if (animstate == animationstate.idle)
+            {
+                if (currentframe < 300)
+                {
+                    tex = idle[0];
+                }
+                else if (currentframe >= 300 && currentframe < 306)
+                {
+                    tex = idle[1];
+                }
+                else if (currentframe >= 306 && currentframe < 300)
+                {
+                    tex = idle[2];
+                }
+                else if (currentframe >= 317 && currentframe < 330)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 330 && currentframe < 342)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 342 && currentframe < 354)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 354 && currentframe < 366)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 366 && currentframe < 378)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 378 && currentframe < 390)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 390 && currentframe < 402)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 402 && currentframe < 414)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 414 && currentframe < 426)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 426 && currentframe < 438)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 438 && currentframe < 450)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 450 && currentframe < 462)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 462 && currentframe < 474)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 474 && currentframe < 486)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 486 && currentframe < 498)
+                {
+                    tex = idle[3];
+                }
+                else if (currentframe >= 498 && currentframe < 510)
+                {
+                    tex = idle[4];
+                }
+                else if (currentframe >= 510 && currentframe < 546)
+                {
+                    tex = idle[5];
+                }
+                else if (currentframe >= 546 && currentframe < 582)
+                {
+                    tex = idle[6];
+                }
+                else if (currentframe >= 582 && currentframe < 588)
+                {
+                    tex = idle[7];
+                }
+                else if (currentframe >= 588 && currentframe < 624)
+                {
+                    tex = idle[8];
+                }
+                else if (currentframe >= 624 && currentframe < 635)
+                {
+                    tex = idle[7];
+                }
+                else
+                {
+                    tex = idle[3];
+                    currentframe = 317;
+                }
+            }
+            if (animstate == animationstate.walking)
+            {
+                if (currentframe < Math.Max(8 - Math.Abs(xsp), 1))
+                {
+                    tex = walking[0];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp),1) && currentframe <= Math.Max(8-Math.Abs(xsp),1)*2)
+                {
+                    tex = walking[1];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*2 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 3)
+                {
+                    tex = walking[2];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*3 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 4)
+                {
+                    tex = walking[3];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*4 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 5)
+                {
+                    tex = walking[4];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*5 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 6)
+                {
+                    tex = walking[5];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*6 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 7)
+                {
+                    tex = walking[6];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1)*7 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 8)
+                {
+                    tex = walking[7];
+                }
+                else
+                {
+                    tex = walking[0];
+                    currentframe = 0;
+                }
+            }
+            else if (animstate == animationstate.running)
+            {
+                if (currentframe < Math.Max(8 - Math.Abs(xsp), 1))
+                {
+                    tex = running[0];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1) && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 2)
+                {
+                    tex = running[1];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1) * 2 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 3)
+                {
+                    tex = running[2];
+                }
+                else if (currentframe >= Math.Max(8 - Math.Abs(xsp), 1) * 3 && currentframe <= Math.Max(8 - Math.Abs(xsp), 1) * 4)
+                {
+                    tex = running[3];
+                }
+                else
+                {
+                    tex = running[0];
+                    currentframe = 0;
+                }
+            }
+            currentframe++;
+        }
+
+        public enum animationstate
+        {
+            idle,
+            walking,
+            running
         }
     }
 }
--- SonicSharp/SonicSharp/Sonic.cs
+++ SonicSharp/SonicSharp/Sonic.cs
@@ -17,7 +17,10 @@ namespace SonicSharp
 
         public void LoadContent(ContentManager Content)
         {
-            running = Content.Load<Texture2D>("Sprites\\running1");
+            walking.AddRange(new Texture2D[] { Content.Load<Texture2D>("Sprites\\walking1"), Content.Load<Texture2D>("Sprites\\walking2"), Content.Load<Texture2D>("Sprites\\walking3"), Content.Load<Texture2D>("Sprites\\walking4"), Content.Load<Texture2D>("Sprites\\walking5"), Content.Load<Texture2D>("Sprites\\walking6"), Content.Load<Texture2D>("Sprites\\walking7"), Content.Load<Texture2D>("Sprites\\walking8") });
+            running.AddRange(new Texture2D[] { Content.Load<Texture2D>("Sprites\\running1"), Content.Load<Texture2D>("Sprites\\running2"), Content.Load<Texture2D>("Sprites\\running3"), Content.Load<Texture2D>("Sprites\\running4")});
+            idle.AddRange(new Texture2D[] { Content.Load<Texture2D>("Sprites\\idle1"), Content.Load<Texture2D>("Sprites\\idle2"), Content.Load<Texture2D>("Sprites\\idle3"), Content.Load<Texture2D>("Sprites\\idle4"), Content.Load<Texture2D>("Sprites\\idle5"), Content.Load<Texture2D>("Sprites\\idle6"), Content.Load<Texture2D>("Sprites\\idle7"), Content.Load<Texture2D>("Sprites\\idle8"), Content.Load<Texture2D>("Sprites\\idle9") });
+            tex = idle[0];
         }
     }
 }
